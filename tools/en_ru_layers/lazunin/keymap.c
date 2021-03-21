03b284285007eac914939b036fd28359a3bccfc1
Vladimir Lazunin <lazunin@gmail.com>
2021-03-21 20:01:21 +0900

    Some refactoring and more distinguishable shift colors.

--- tools/en_ru_layers/lazunin/keymap.c
+++ tools/en_ru_layers/lazunin/keymap.c
@@ -2,17 +2,25 @@
 #include "version.h"
 #include "keymap_russian.h"
 
+enum layers
+{
+    EN_LO,
+    EN_HI,
+    RU_LO,
+    RU_HI,
+    SYMB
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_moonlander(
+  [EN_LO] = LAYOUT_moonlander(
 	KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_EXLM,                                        KC_QUOTE,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
 	KC_PSCREEN,     KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_SLASH,                                       KC_DOT,         KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
-	KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_BSLASH,                                                                      KC_COMMA,       KC_H,           KC_J,           KC_K,           KC_L,           KC_TRANSPARENT, KC_TRANSPARENT, 
-	TT(1),          KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TT(1),          
-	TG(2),          KC_LCTRL,       KC_LALT,        LCTL(LSFT(KC_1)),LCTL(LSFT(KC_2)),KC_LGUI,                                                                                                        KC_ENTER,       KC_UP,          KC_DOWN,        KC_RALT,        KC_RCTRL,       TG(4),          
-	KC_SPACE,       KC_TAB,         MO(4),                          MO(4),          KC_DELETE,      KC_BSPACE
+	KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_BSLASH,                                      KC_COMMA,       KC_H,           KC_J,           KC_K,           KC_L,           KC_TRANSPARENT, KC_TRANSPARENT, 
+	TT(1),          KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                                           KC_N,           KC_M,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TT(1),          
+	TG(2),          KC_LCTRL,       KC_LALT,        KC_LEFT,        KC_RIGHT,       KC_LGUI,                                                                        KC_ENTER,       KC_UP,          KC_DOWN,        KC_RALT,        KC_RCTRL,       TG(4),          
+	                                                                                KC_SPACE,       KC_TAB,         MO(4),                          MO(4),          KC_DELETE,      KC_BSPACE
   ),
-  [1] = LAYOUT_moonlander(
+  [EN_HI] = LAYOUT_moonlander(
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_QUES,                                        KC_DQUO,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_UNDS,        
 	KC_TRANSPARENT, LSFT(KC_Q),     LSFT(KC_W),     LSFT(KC_E),     LSFT(KC_R),     LSFT(KC_T),     KC_TRANSPARENT,                                 KC_COLN,        LSFT(KC_Y),     LSFT(KC_U),     LSFT(KC_I),     LSFT(KC_O),     LSFT(KC_P),     KC_TRANSPARENT, 
 	KC_TRANSPARENT, LSFT(KC_A),     LSFT(KC_S),     LSFT(KC_D),     LSFT(KC_F),     LSFT(KC_G),     KC_TRANSPARENT,                                                                 KC_SCOLON,      LSFT(KC_H),     LSFT(KC_J),     LSFT(KC_K),     LSFT(KC_L),     KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -20,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
-  [2] = LAYOUT_moonlander(
+  [RU_LO] = LAYOUT_moonlander(
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RU_EXLM,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RU_MINS,        
 	KC_TRANSPARENT, RU_SHTI,        RU_TSE,         RU_U,           RU_KA,          RU_IE,          RU_SLSH,                                        RU_DOT,         RU_EN,          RU_GHE,         RU_SHA,         RU_SHCH,        RU_ZE,          RU_HA,          
 	KC_TRANSPARENT, RU_EF,          RU_YERU,        RU_VE,          RU_A,           RU_PE,          RU_BSLS,                                                                        RU_COMM,        RU_ER,          RU_O,           RU_EL,          RU_DE,          RU_ZHE,         RU_E,           
@@ -28,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
-  [3] = LAYOUT_moonlander(
+  [RU_HI] = LAYOUT_moonlander(
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RU_QUES,                                        RU_DQUO,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RU_UNDS,        
 	KC_TRANSPARENT, LSFT(RU_SHTI),  LSFT(RU_TSE),   LSFT(RU_U),     LSFT(RU_KA),    LSFT(RU_IE),    KC_TRANSPARENT,                                 RU_COLN,        LSFT(RU_EN),    LSFT(RU_GHE),   LSFT(RU_SHA),   LSFT(RU_SHCH),  LSFT(RU_ZE),    LSFT(RU_HA),    
 	KC_TRANSPARENT, LSFT(RU_EF),    LSFT(RU_YERU),  LSFT(RU_VE),    LSFT(RU_A),     LSFT(RU_PE),    KC_TRANSPARENT,                                                                 RU_SCLN,        LSFT(RU_ER),    LSFT(RU_O),     LSFT(RU_EL),    LSFT(RU_DE),    LSFT(RU_ZHE),   LSFT(RU_E),     
@@ -36,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
 	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
-  [4] = LAYOUT_moonlander(
+  [SYMB] = LAYOUT_moonlander(
 	KC_PLUS,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_ASTR,                                        KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
 	KC_MINUS,       KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_PGUP,        KC_HOME,        KC_UP,          KC_END,         KC_TRANSPARENT, KC_F12,         
 	KC_EQUAL,       KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,       KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_PGDOWN,      KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_INSERT,      
@@ -57,46 +65,33 @@ void set_layer_color(int layer)
 {
     switch(layer)
     {
-        case 0: 
+        case EN_LO: 
             rgb_matrix_set_color_all(0, 0, 127);
             break;
-        case 1: 
-            rgb_matrix_set_color_all(0, 0, 255);
+        case EN_HI: 
+            rgb_matrix_set_color_all(63, 63, 255);
             break;
-        case 2: 
+        case RU_LO: 
             rgb_matrix_set_color_all(0, 127, 0);
             break;
-        case 3: 
-            rgb_matrix_set_color_all(0, 255, 0);
+        case RU_HI: 
+            rgb_matrix_set_color_all(63, 255, 63);
             break;
-        case 4: 
+        case SYMB: 
             rgb_matrix_set_color_all(127, 0, 0);
             break;
     }
 }
 
-void rgb_matrix_indicators_user(void) {
-  if (g_suspend_state || keyboard_config.disable_layer_led) { return; }
-  switch (biton32(layer_state)) {
-	case 0:
-	  set_layer_color(0);
-	  break;
-	case 1:
-	  set_layer_color(1);
-	  break;
-	case 2:
-	  set_layer_color(2);
-	  break;
-	case 3:
-	  set_layer_color(3);
-	  break;
-	case 4:
-	  set_layer_color(4);
-	  break;
-   default:
-	if (rgb_matrix_get_flags() == LED_FLAG_NONE)
-	  rgb_matrix_set_color_all(0, 0, 0);
-	break;
+void rgb_matrix_indicators_user(void) 
+{
+  if (g_suspend_state || keyboard_config.disable_layer_led) return;
+  
+  uint8_t lstate = biton32(layer_state);
+  if ( (lstate >= EN_LO) && (lstate <= SYMB)) set_layer_color(lstate);
+  else
+  {
+      if (rgb_matrix_get_flags() == LED_FLAG_NONE) rgb_matrix_set_color_all(0, 0, 0);
   }
 }
 
