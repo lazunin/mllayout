299ccc5ee40841cfec5d23c02bf8ddd9b309021c
Vladimir Lazunin <lazunin@gmail.com>
2021-03-22 23:08:41 +0900

    My layered quasi-shift now works more like real shift. Some layout gui tweaks.

--- tools/en_ru_layers/lazunin/keymap.c
+++ tools/en_ru_layers/lazunin/keymap.c
@@ -138,7 +138,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     		unregister_code(KC_LCTRL);
             break;
         }
+    case TT(EN_HI):                         // to keep Shift pressed
+    case TT(RU_HI):
+        if (record->event.pressed)          // press Shift
+        {
+            register_code(KC_LSHIFT);
+        }
+        else                                // release Shift
+        {
+            unregister_code(KC_LSHIFT);
+        }
   }
   return true;
 }
-
